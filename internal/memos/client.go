0cef85a3c8936355f7744c73481e5b1c5ec9ac9a
Athurg Feng <athurg@gooth.org>
2023-05-23 10:02:25 +0800

    Add http request wrapper

--- internal/memos/client.go
+++ internal/memos/client.go
@@ -1,5 +1,15 @@
 package memos
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
 type Client struct {
 	OpenId string
 	Addr   string
@@ -9,3 +19,54 @@ func New(addr, openId string) *Client {
 	return &Client{Addr: addr, OpenId: openId}
 }
 
+func (c *Client) request(method, apiPath string, query url.Values, param, result any) error {
+	apiUrl := c.Addr + apiPath
+	if query == nil {
+		query = url.Values{}
+	}
+	if c.OpenId != "" {
+		query.Set("openId", c.OpenId)
+	}
+	if q := query.Encode(); q != "" {
+		apiUrl += "?" + q
+	}
+
+	buff, err := json.Marshal(param)
+	if err != nil {
+		return fmt.Errorf("fail to marshal param %v: %s", param, err)
+	}
+
+	req, err := http.NewRequest(method, apiUrl, bytes.NewReader(buff))
+	if err != nil {
+		return fmt.Errorf("fail to create http.Request: %s", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("fail to do http.Request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	buff, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("fail to read http.Response: %s", err)
+	}
+
+	var respInfo struct {
+		Error   string
+		Message string
+		Data    any
+	}
+	respInfo.Data = result
+
+	err = json.Unmarshal(buff, &respInfo)
+	if err != nil {
+		return fmt.Errorf("fail to unmarshal http.Response %s: %s", string(buff), err)
+	}
+
+	if respInfo.Error != "" {
+		return errors.New(respInfo.Error)
+	}
+
+	return nil
+}
