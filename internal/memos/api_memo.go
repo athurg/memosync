678e4b0b872621c1ced5b8d5d6a32165ee7d39ae
Athurg Feng <athurg@gooth.org>
2023-07-06 17:45:06 +0800

    Add support for fetch memo from specified user instead of all users

--- internal/memos/api_memo.go
+++ internal/memos/api_memo.go
@@ -30,6 +30,25 @@ func (c *Client) MemoList(offset, limit int) ([]Memo, error) {
 	return respInfo.Data, nil
 }
 
+// UserMemoList fetch memo list of specified user from memos server
+func (c *Client) UserMemoList(userId, offset, limit int) ([]Memo, error) {
+	query := url.Values{
+		"creatorId": {strconv.Itoa(userId)},
+		"offset":    {strconv.Itoa(offset)},
+		"limit":     {strconv.Itoa(limit)},
+	}
+
+	var respInfo struct {
+		Data []Memo
+	}
+	err := c.request("GET", "/api/memo", query, nil, &respInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return respInfo.Data, nil
+}
+
 // CreateMemo create memo from exists one
 func (c *Client) CreateMemo(content string, createdTs int64, resourceIds []int) (*Memo, error) {
 	param := MemoCreate{
--- sync_memo.go
+++ sync_memo.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"memosync/internal/memos"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 // TODO:
@@ -32,19 +35,44 @@ func syncMemos() {
 
 // syncTargetToUser sync target memos server into user's memos
 func syncTargetToUser(u memos.User) error {
-	log.Printf("Sync UserID=%d for %s", u.ID, u.Username)
+	srcUrl, err := url.Parse(u.Username)
+	if err != nil {
+		return fmt.Errorf("fail to url.Parse %s: %s", u.Username, err)
+	}
 
-	hostSvr := memos.New(addr, u.OpenID)
-	srcSvr := memos.New(u.Username, "")
+	// only one user
+	var srcUserID int
+	if srcUrl.Path != "/" && srcUrl.Path != "" {
+		if !strings.HasPrefix(srcUrl.Path, "/u/") {
+			return fmt.Errorf("invalid url %s: path should empty or start with /u/", u.Username)
+		}
+		userIDStr := strings.TrimPrefix(srcUrl.Path, "/u/")
+		userID, err := strconv.Atoi(userIDStr)
+		if err != nil {
+			return fmt.Errorf("fail to parse userid from %s:%s", userIDStr, err)
+		}
+		srcUserID = userID
+	}
 
-	memos, err := srcSvr.MemoList(0, maxPerLoop)
+	srcAddr := srcUrl.Scheme + "://" + srcUrl.Host
+	srcSvr := memos.New(srcAddr, "")
+
+	var allMemos []memos.Memo
+	if srcUserID > 0 {
+		log.Printf("Sync UserID=%d for user=%d of %s", u.ID, srcUserID, srcAddr)
+		allMemos, err = srcSvr.UserMemoList(srcUserID, 0, maxPerLoop)
+	} else {
+		log.Printf("Sync UserID=%d for all user of %s", u.ID, srcAddr)
+		allMemos, err = srcSvr.MemoList(0, maxPerLoop)
+	}
 	if err != nil {
 		return fmt.Errorf("fail to fetch memos: %s", err)
 	}
 
 	var skipCount, resourceCount int
 
-	for _, memo := range memos {
+	hostSvr := memos.New(addr, u.OpenID)
+	for _, memo := range allMemos {
 		// Skip memos already synced
 		if memo.CreatedTs < lastCheckTs {
 			skipCount += 1
@@ -71,7 +99,7 @@ func syncTargetToUser(u memos.User) error {
 		}
 	}
 
-	log.Printf("Total %d memos, create %d memos with %d resources", len(memos), len(memos)-skipCount, resourceCount)
+	log.Printf("Total %d memos, create %d memos with %d resources", len(allMemos), len(allMemos)-skipCount, resourceCount)
 
 	return nil
 }
